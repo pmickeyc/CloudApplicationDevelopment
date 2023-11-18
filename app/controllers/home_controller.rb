49e2f66e52ab1619649365a699ff8512bb6f1914
Padraig McCauley <63427903+pmickeyc@users.noreply.github.com>
2023-11-18 12:39:24 +0000

    Adding homepage

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    root 'home#index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :sales_lines
   resources :sales_headers
   resources :items
