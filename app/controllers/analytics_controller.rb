842541a20eff3fa76502f9501a4aa3bda62624d2
Padraig McCauley <63427903+pmickeyc@users.noreply.github.com>
2023-12-08 22:26:34 +0000

    Formatting fixes and Analytics filter change

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,14 +6,14 @@ class AnalyticsController < ApplicationController
 
     @item_names = OrderLine.joins(:item).distinct.pluck('items.name')
 
-    # Filter by item
-    if params[:item].present?
-      @orders = @orders.joins(:order_lines).where('order_lines.item.name LIKE ?', "%#{params[:item]}%")
+    # Filter by date range
+    if params[:start_date].present? && params[:end_date].present?
+      start_date = Date.parse(params[:start_date])
+      end_date = Date.parse(params[:end_date])
+      @orders = @orders.where("DATE(transaction_date) BETWEEN ? AND ?", start_date, end_date)
+      @email_logs = EmailLog
+        .where("DATE(sent_at) BETWEEN ? AND ?", start_date, end_date)
+        .order(sent_at: :desc)
     end
-
-    # Filter by date
-    return unless params[:date].present?
-
-    @orders = @orders.where('transaction_date = ?', Date.parse(params[:date]))
   end
 end
